92ac3671097b092ba088891e61e80ef306bccf3c
Syed Yarooq <syed.yarooq@gmail.com>
2022-10-02 03:57:33 +0500

    remove cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import express from "express";
-import cors from 'cors';
 import config from "./config/config";
 import indexRouter from './routes/index-router';
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use('/', indexRouter);
-
-
 
 const port = config.api.port || 3000;
 
