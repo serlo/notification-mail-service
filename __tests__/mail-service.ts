c4effca78f389adb0588c70dbe04e07ae4bec2f9
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-11-04 18:44:14 +0100

    style: changes by formatter

--- __tests__/mail-service.ts
+++ __tests__/mail-service.ts
@@ -19,7 +19,7 @@ const expectedResponse = [
     user_id: 1,
     username: 'user',
     email: 'email@serlo.org',
-    ids: ['12','3'],
+    ids: ['12', '3'],
     body: '<p>actor created taxonomy term on  created 2022-10-11 11:30</p><br/><p>actor2 created taxonomy term on  created 2022-10-11 11:50</p><br/>',
   },
 ]
