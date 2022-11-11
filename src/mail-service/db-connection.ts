fd8592c74e9a2a7320657c0fa9779af695aad32f
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-11-11 10:18:23 +0100

    refactor: clean up a little bit more

--- src/mail-service/db-connection.ts
+++ src/mail-service/db-connection.ts
@@ -1,17 +1,21 @@
 // eslint-disable-next-line import/no-internal-modules
 import { Connection } from 'mysql2/promise'
 
-import {
-  EmailData,
-  EmailPayload,
-  eventMessages,
-  EventType,
-  formattedDate,
-} from './utils'
+import { EmailPayload, eventMessages, EventType, formattedDate } from './utils'
 
 export interface DBConnection {
   getAllUnsentEmailData(): Promise<EmailPayload[]>
   updateNotificationSendStatus(notificationsIds: string[]): Promise<void>
+}
+
+interface EmailData {
+  user_id: number
+  username: string
+  email: string
+  event_ids: string
+  dates: string
+  actor_names: string
+  notification_ids: string
 }
 
 export class MysqlConnection implements DBConnection {
@@ -24,18 +28,18 @@ export class MysqlConnection implements DBConnection {
   async getAllUnsentEmailData() {
     const [rows] = await this.connection.execute(
       `SELECT GROUP_CONCAT(notification.id) as notification_ids, notification.user_id, user.username, user.email, 
-              GROUP_CONCAT(event_log.event_id) as event_ids, GROUP_CONCAT(event_log.date) as dates, GROUP_CONCAT(actor.username) as actor_names          
-            FROM notification
-            INNER JOIN user ON user.id = notification.user_id 
-            INNER JOIN notification_event ON notification.id = notification_event.notification_id
-            INNER JOIN event_log ON event_log.id = notification_event.event_log_id
-            JOIN event ON event.id = event_log.event_id
-            LEFT JOIN event_parameter ON event_parameter.log_id =  event_log.id
-            LEFT JOIN event_parameter_uuid ON event_parameter_uuid.event_parameter_id = event_parameter.id
-            INNER JOIN user actor ON event_log.actor_id = actor.id
-            INNER JOIN subscription ON subscription.user_id = user.id AND event_parameter_uuid.uuid_id = subscription.uuid_id
-            WHERE notification.email = 1 AND notification.email_sent = 0 AND notification.seen = 0
-            GROUP BY notification.user_id;`
+          GROUP_CONCAT(event_log.event_id) as event_ids, GROUP_CONCAT(event_log.date) as dates, GROUP_CONCAT(actor.username) as actor_names          
+        FROM notification
+        INNER JOIN user ON user.id = notification.user_id 
+        INNER JOIN notification_event ON notification.id = notification_event.notification_id
+        INNER JOIN event_log ON event_log.id = notification_event.event_log_id
+        JOIN event ON event.id = event_log.event_id
+        LEFT JOIN event_parameter ON event_parameter.log_id =  event_log.id
+        LEFT JOIN event_parameter_uuid ON event_parameter_uuid.event_parameter_id = event_parameter.id
+        INNER JOIN user actor ON event_log.actor_id = actor.id
+        INNER JOIN subscription ON subscription.user_id = user.id AND event_parameter_uuid.uuid_id = subscription.uuid_id
+        WHERE notification.email = 1 AND notification.email_sent = 0 AND notification.seen = 0
+        GROUP BY notification.user_id;`
     )
     return this.prepareEmailPayload(rows as EmailData[])
   }
@@ -46,19 +50,20 @@ export class MysqlConnection implements DBConnection {
     const emailPayloads: EmailPayload[] = emailData.map((data) => {
       const dates = data.dates.split(',')
       const event_ids = data.event_ids.split(',')
-      const notification_ids = data.notification_ids.split(',')
       const actor_names = data.actor_names.split(',')
-      let body = ''
-      actor_names.forEach((actor: string, i: number) => {
-        body = `${body}<p>${actor} ${
-          eventMessages[parseInt(event_ids[i]) as EventType]
-        } ${formattedDate(new Date(dates[i]))}</p><br/>`
-      })
+      // Actually, the construction of the body should go to somewhere else
+      const body = actor_names
+        .map((actor, i) => {
+          return `<p>${actor} ${
+            eventMessages[parseInt(event_ids[i]) as EventType]
+          } ${formattedDate(new Date(dates[i]))}</p><br/>`
+        })
+        .toString()
       return {
         user_id: data.user_id,
         username: data.username,
         email: data.email,
-        ids: notification_ids,
+        ids: data.notification_ids.split(','),
         body,
       }
     })
@@ -69,10 +74,8 @@ export class MysqlConnection implements DBConnection {
   async updateNotificationSendStatus(notificationsIds: string[]) {
     await this.connection.query(
       `UPDATE notification
-              SET email_sent = true
-              WHERE id in (${String(
-                notificationsIds.join().replace("'", ' ')
-              )});`
+        SET email_sent = true
+        WHERE id in (${String(notificationsIds.join().replace("'", ' '))});`
     )
   }
 }
