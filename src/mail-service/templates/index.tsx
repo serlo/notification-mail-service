35c90101a274b017f98f0eb34b93766c404181d5
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-12-23 15:15:31 +0100

    refactor: remove unused lib

--- src/mail-service/templates/index.tsx
+++ src/mail-service/templates/index.tsx
@@ -1,11 +1,3 @@
-import moment from 'moment'
-
 export * from './components/notification-mail'
 
 export const domain = 'https://serlo.org'
-
-// TODO: investigate if we need it
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function formattedDate(date: Date) {
-  return moment(date).format('YYYY-MM-DD HH:mm')
-}
