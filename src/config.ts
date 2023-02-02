4b29b6fd3cfe1b05678abc79dffdca6553162ad5
Stephan Kulla <git.mail@kulla.me>
2023-02-02 18:49:40 +0100

    refactor: Do not call dotenv.config() in production mode

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 
-dotenv.config()
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 
 export const config = {
   serloApiGraphqlUrl: process.env.SERLO_API_GRAPHQL_URL,
