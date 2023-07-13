dad0133655e4099e7837d6a8cf1d9fe046e60a2f
Stephan Kulla <git.mail@kulla.me>
2023-07-13 15:07:42 +0200

    test: jest.config.json -> jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
-{
+import type { Config } from '@jest/types'
+
+const jestConfig: Config.InitialOptions = {
   "preset": "ts-jest/presets/default-esm",
   "setupFiles": ["dotenv/config"],
   "setupFilesAfterEnv": ["<rootDir>/jest.setup.ts"],
   "testEnvironment": "node",
   "testPathIgnorePatterns": ["/node_modules/"]
 }
+
+export default jestConfig
