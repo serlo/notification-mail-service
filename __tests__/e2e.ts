218da7b1d651fc27d57498b7d61deae7fd0abe81
Stephan Kulla <git.mail@kulla.me>
2023-07-21 20:35:01 +0200

    test: Mock API for e2e test
    Fixes #36

--- __tests__/e2e.ts
+++ __tests__/e2e.ts
@@ -1,10 +1,44 @@
+import express from 'express'
 import { GraphQLClient } from 'graphql-request'
+import type { Server } from 'http'
+import * as t from 'io-ts'
 import { createConnection } from 'mysql2/promise'
 import { createTransport } from 'nodemailer'
 
+import { apiReponses } from '../__fixtures__/api-responses'
 import { config } from '../src/config'
 import { notifyUsers, MysqlConnection } from '../src/mail-service'
 import { createToken } from '../src/utils'
+
+const apiPort = 3003
+let api: ReturnType<typeof express>
+let server: Server
+
+beforeAll((done) => {
+  api = express()
+  api.use(express.json())
+
+  api.post('/graphql', (req, res) => {
+    if (!t.type({ variables: t.type({ userId: t.number }) }).is(req.body)) {
+      res.sendStatus(400)
+      return
+    }
+
+    const data = apiReponses[req.body.variables.userId]
+
+    if (data != null) {
+      res.json({ data })
+    } else {
+      res.sendStatus(400)
+    }
+  })
+
+  server = api.listen(apiPort, done)
+})
+
+afterAll((done) => {
+  server.close(done)
+})
 
 test('should send all emails and set notifications as sent', async () => {
   const connection = await createConnection(config.dbUri)
@@ -18,9 +52,10 @@ test('should send all emails and set notifications as sent', async () => {
   })
 
   const token = createToken({ secret: config.serloApi.sharedSecret })
-  const apiGraphqlClient = new GraphQLClient(config.serloApi.graphqlUrl, {
-    headers: { Authorization: `Serlo Service=${token}` },
-  })
+  const apiGraphqlClient = new GraphQLClient(
+    `http://localhost:${apiPort}/graphql`,
+    { headers: { Authorization: `Serlo Service=${token}` } },
+  )
 
   const firstResults = await notifyUsers(
     mysqlConnection,
