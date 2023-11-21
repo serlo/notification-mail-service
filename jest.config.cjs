d4f4a5389400afb79c695a6f609b93e874ba93eb
Stephan Kulla <git.mail@kulla.me>
2023-11-22 00:19:28 +0100

    chore(jest): Use CommonJS for faster startup of jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,8 @@
-import type { Config } from '@jest/types'
-
-const jestConfig: Config.InitialOptions = {
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
   preset: 'ts-jest/presets/default-esm',
   setupFiles: ['dotenv/config'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/'],
 }
-
-/* eslint-disable import/no-default-export */
-export default jestConfig
